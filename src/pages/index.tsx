345e360e3b4f9bc4bd73fac51dc64ce801fa0cdb
Skolepraktik V18 - Kunde test <85283941+v18kunde@users.noreply.github.com>
2021-06-03 14:29:10 +0200

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,10 +13,10 @@ export default function Index() {
       <div className="container">
         <div>
           <h1>
-            Hi, We're Next.js & Netlify<span className="fancy">.</span>
+            Lavet med Next.js & Netlify<span className="fancy">.</span>
           </h1>
           <span className="handle">@nextjs-netlify-blog</span>
-          <h2>A blog template with Next.js and Netlify.</h2>
+          <h2>Skolepraktik test med Next.js and Netlify.</h2>
           <SocialList />
         </div>
       </div>
